0a2b523fadd600869fd2afb5cafd17f6a1577e9e
Pol Gasull <pol@innroute.com>
2018-11-24 12:52:17 +0100

    Angular 7 first step

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  h1Style = false;
+  users: Object;
+
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.data.getUsers().subscribe(data => {
+      this.users = data;
+      console.log(this.users);
+    });
+  }
+
+  firstClick() {
+    this.h1Style = true;
+  }
+}
